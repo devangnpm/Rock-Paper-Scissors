466ce7d2e620aa395c93a3fd74bb41ac4491da9d
Devang Yadav <d3vang2002@gmail.com>
2023-12-22 20:08:40 +0530

    created a game() func and merged it with playRound() func to play 5 rounds and declare the winner

--- script.js
+++ script.js
@@ -4,26 +4,48 @@ function getComputerChoice(params) {
 
   return strings[randomIndex];
 }
-const result = getComputerChoice();
 
-// function getPlayerChoice(params) {
+let playerScore = 0;
+let computerScore = 0;
+const gameResults = game();
+console.log(gameResults);
 
-// }
+function game() {
+let result = "";
 
-const playerSelection = "paper";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+  for (let round = 0; round < 5; round++) {
+  const playerSelection = prompt("Select your choice from Rock,Paper or Scissor");
+  const playerSelectionLower = playerSelection.toLowerCase();
+  const computerSelection = getComputerChoice();
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
+    playRound(playerSelectionLower,computerSelection);
+   console.log(playerScore, computerScore);
+    if (playerScore === 3) {
+      result = "Player Wins"
+      return result;
+    }
+    else if (computerScore === 3) {
+      result = "Computer Wins!"
+      return result;
+    }
+    else if (computerScore && playerScore === 3) {
+      result = "Match Tied!"
+    }
+  }
+} 
+
+function playRound(playerSelectionLower, computerSelection) {
+  if (playerSelectionLower === computerSelection) {
     return "It's a tie";
   } else if (
-    (playerSelection == "rock" && computerSelection == "scissors") ||
-    (playerSelection == "paper" && computerSelection == "rock") ||
-    (playerSelection == "scissors" && computerSelection == "paper")
+    (playerSelectionLower == "rock" && computerSelection == "scissors") ||
+    (playerSelectionLower == "paper" && computerSelection == "rock") ||
+    (playerSelectionLower == "scissors" && computerSelection == "paper")
   ) {
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
+    playerScore++;
+    return `You Win! ${playerSelectionLower} beats ${computerSelection}`;
   } else {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    computerScore++
+    return `You Lose! ${computerSelection} beats ${playerSelectionLower}`;
   }
 }
